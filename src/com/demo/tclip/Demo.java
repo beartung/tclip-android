8c2759bf53690047da0af53b05a039ad8ff0c554
Bear <beartung@gmail.com>
2013-11-27 17:25:03 +0800

    test gray

--- src/com/demo/tclip/Demo.java
+++ src/com/demo/tclip/Demo.java
@@ -2,22 +2,77 @@ package com.demo.tclip;
 
 import android.app.Activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import android.os.Bundle;
+
+import android.util.Log;
+
+import android.widget.ImageView;
 
 import com.opencv.TClip;
 
-public class Demo extends Activity
-{
-    /** Called when the activity is first created. */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
+public class Demo extends Activity {
+
+    private static final String TAG = "Demo";
+    private ImageView img;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        img = (ImageView)findViewById(R.id.img);
     }
+
+    private BaseLoaderCallback callback = new BaseLoaderCallback(this){
+
+        public void onManagerConnected(int status) {
+
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    Bitmap src = getPic();
+                    Bitmap gray = TClip.gray(src);
+                    img.setImageBitmap(gray);
+                    src.recycle();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+
+            }
+        }
+
+    };
 
     public void onResume(){
         super.onResume(); 
         TClip.crop_test();
+
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_7, this, callback);
     }
+
+    private Bitmap getPic(){
+        File f = new File("/sdcard/a1.jpg"); 
+        Bitmap ret = null;
+        try {
+            InputStream stream = new FileInputStream(f);
+            ret = BitmapFactory.decodeStream(stream);
+            if (stream != null) stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
 }
--- src/com/opencv/TClip.java
+++ src/com/opencv/TClip.java
@@ -1,8 +1,69 @@
 package com.opencv;
 
+import android.content.Context;
+
+import android.graphics.Bitmap;
+
+import android.util.Log;
+
 import java.lang.UnsatisfiedLinkError;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
 public class TClip {
+
+    private static final String TAG = "TClipJAVA";
+
+    public static void init(Context context){
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_7, context, new BaseLoaderCallback(context){
+
+            public void onManagerConnected(int status) {
+
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS:
+                        Log.i(TAG, "OpenCV loaded successfully");
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+
+                }
+            }
+
+        });
+    }
+
+    public static Bitmap gray(Bitmap src){
+        Bitmap ret = null;
+
+        if (src == null){
+            return ret;
+        }
+
+        try{
+            Mat mat = new Mat(src.getHeight(), src.getWidth(), CvType.CV_8U, new Scalar(4));
+            Utils.bitmapToMat(src, mat);
+
+            //Bitmap copy = src.copy(Bitmap.Config.ARGB_8888, true);
+            //Utils.bitmapToMat(copy, mat);
+
+            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY, 4);
+
+            ret = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(mat, ret);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
 
     public static native void crop_test();
 
